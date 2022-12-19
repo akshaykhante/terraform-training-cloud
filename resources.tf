a60ec539f7fa68fc2ef2befa80c569d7280416df
unknown <akshaykhante25@gmail.com>
2022-12-19 14:23:13 +0530

    added azs for subnet

--- resources.tf
+++ resources.tf
@@ -7,6 +7,7 @@ resource "aws_vpc" "test_vpc_ak" {
 resource "aws_subnet" "test_subnet_ak" {
   vpc_id = aws_vpc.test_vpc_ak.id
   for_each = toset(data.aws_availability_zones.us_east_1_azs.names)
+  availability_zone = each.value
   cidr_block = lookup(var.cidr_blocks_subnet, each.value)
   tags = { "Name" = format("ak-subnet-%s",each.value)}
 }

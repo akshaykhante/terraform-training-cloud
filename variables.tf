b0bf938a01735aaf1ce635a2067b9a8e6b50931a
unknown <akshaykhante25@gmail.com>
2022-12-19 13:04:22 +0530

    removed var

--- variables.tf
+++ variables.tf
@@ -1,8 +1,4 @@
 
-variable "servers_name" {
-  type = list(string)
-}
-
 variable "cidr_blocks_subnet" {
   type = map(string)
   default = {"us-east-1a" = "10.30.1.0/24","us-east-1b" = "10.30.2.0/24", "us-east-1c" = "10.30.3.0/24", "us-east-1d" = "10.30.4.0/24", "us-east-1e" = "10.30.5.0/24", "us-east-1f" = "10.30.6.0/24" }
--- variables.tfvars
+++
@@ -1,3 +0,0 @@
-inatsnce_ami = "ami-0b0dcb5067f052a63"
-instance_type_var = "t2.micro"
-servers_name = [ "ak_server_1", "ak_server_2", "ak_server_3" ]
